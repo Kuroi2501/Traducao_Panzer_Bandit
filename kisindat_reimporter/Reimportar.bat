15e903b1e41ff6d971f09c23cbe221e3ee447534
Kuroi2501 <kuroi2502@gmail.com>
2024-08-08 01:31:48 -0300

    Adicionados novos graficos

--- kisindat_reimporter/Reimportar.bat
+++ kisindat_reimporter/Reimportar.bat
@@ -1,6 +1,6 @@
 xcopy "..\Binarios\*.*" "output2\" /s /f /Y
 xcopy "..\Arquivos_Profile\bin\*.*" "output2\" /s /f /Y
-xcopy "..\Graficos\KIFONT.SP" "output2\" /s /f /Y
+xcopy "..\Graficos\bin\*.*" "output2\" /s /f /Y
 xcopy "..\Staff\STAFF.SP" "output2\" /s /f /Y
 python kisindat_extractor.py r SLPS_008.99 KISINDAT.NCB .\output2
 pause
